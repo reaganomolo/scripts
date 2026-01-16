adff60fe2b89b262bcfd3ad574cf81f477480af6
reaganhat <komoloreagan@gmail.com>
2026-01-16 15:26:22 +0300

    mofified file.sh to check the existence of a dir befere creating one with the same name

--- file.sh
+++ file.sh
@@ -4,17 +4,28 @@
 read -p "Enter name of the new directory: " name
 
 # 2. Exit if the user didn't type anything (Safety first!)
+#if [[ -z "$name" ]]; then
+ #  exit 1
+#fi
+
+# The above block of code could also be achieved by this single line:
 if [[ -z "$name" ]]; then
-    echo "Error: No name entered."
-    exit 1
+ echo "No name provided, aborting." >&2
+ exit 1
 fi
+
+if [[ -d "$name" ]]; then
+ echo "$name already exists, aborting." >&2
+ exit 1
+fi
+mkdir -p "$name"
 
 # 3. Create and enter directory (Using quotes for safety)
 # Using quotes enables name variable to accept directory names with spacing instead\
 # Of creating multiple directories ie if the intention is to create one single directory.
 # Enclosing the "name" in quotes enables it to interpret the input as one directory name.
 
-mkdir -p "$name"
+#mkdir -p "$name"
 cd "$name" || exit # if changing into the new directory fails by using ||, the program exits.
 echo "Current directory: $(pwd)"
 
