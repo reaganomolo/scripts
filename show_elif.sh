0771086db2ce463f603da33091e8013df19a1a29
reaganhat <komoloreagan@gmail.com>
2026-01-18 12:39:50 +0300

    editted file: showelif.sh

--- show_elif.sh
+++ show_elif.sh
@@ -3,12 +3,12 @@
 read -p "ENTER YOUR NAME: " name
 
 if [ -z "$name" ]; then
-    echo "ERROR: Name cannot be empty."
+    echo "ERROR: Name cannot be empty." #Shows an error if the input is empty
     exit 1
 elif [ "$name" == "Alice" ]; then
     echo "Hello, "$name"! Welcome back!"
 elif [ "$name" == "Bob" ]; then
     echo "Hi "$name"! Good to see you!"
 else
-    echo "Hello, $name! Nice to meet you!"
+    echo "Hello, $name! Nice to meet you!" #Greets the user with a generic message for any other name
 fi
